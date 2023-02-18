bb4793359245109f5579c492b1df35eab6ed779b
terry-u16 <terry-u16@users.noreply.github.com>
2023-02-18 12:04:46 +0900

    Initial commit

---
+++ src/bin/a.rs
@@ -0,0 +1,38 @@
+#[allow(unused_imports)]
+use proconio::*;
+#[allow(unused_imports)]
+use rand::prelude::*;
+
+pub trait ChangeMinMax {
+    fn change_min(&mut self, v: Self) -> bool;
+    fn change_max(&mut self, v: Self) -> bool;
+}
+
+impl<T: PartialOrd> ChangeMinMax for T {
+    fn change_min(&mut self, v: T) -> bool {
+        *self > v && {
+            *self = v;
+            true
+        }
+    }
+
+    fn change_max(&mut self, v: T) -> bool {
+        *self < v && {
+            *self = v;
+            true
+        }
+    }
+}
+
+#[allow(unused_macros)]
+macro_rules! mat {
+    ($e:expr; $d:expr) => { vec![$e; $d] };
+    ($e:expr; $d:expr $(; $ds:expr)+) => { vec![mat![$e $(; $ds)*]; $d] };
+}
+
+#[derive(Debug, Clone)]
+struct Input {}
+
+fn main() {
+    todo!();
+}
