3aa27616e838fa1c9b4f5794bc55cdd02f01276f
terry-u16 <terry-u16@users.noreply.github.com>
2023-02-25 21:59:39 +0900

    ハイパラ調整を外出し

--- src/gaussean_process.rs
+++ src/gaussean_process.rs
@@ -5,6 +5,7 @@ use nalgebra::{DMatrix, DVector};
 pub struct GaussianPredictor {
     x_list: Vec<DVector<f64>>,
     y_list: Vec<f64>,
+    params: GaussianParam,
 }
 
 impl GaussianPredictor {
@@ -12,6 +13,7 @@ impl GaussianPredictor {
         Self {
             x_list: vec![],
             y_list: vec![],
+            params: GaussianParam::new(1.0, 1.0, 1.0),
         }
     }
 
@@ -21,22 +23,12 @@ impl GaussianPredictor {
     }
 
     pub fn gaussian_process_regression(&self, x_pred: &DMatrix<f64>) -> (Vec<f64>, Vec<f64>) {
-        let x_train = DMatrix::from_row_slice(
-            self.x_list.len(),
-            self.x_list[0].len(),
-            &self.x_list.iter().flatten().copied().collect_vec(),
-        );
-        let mut y_train = DVector::from_vec(self.y_list.clone());
-
-        // 平均を引く
-        let y_average = y_train.mean();
-        y_train.add_scalar_mut(-y_average);
+        let (x_train, y_train, y_average) = self.preprocess();
 
         let train_len = self.x_list.len();
         let pred_len = x_pred.len();
 
-        let (t1, t2, t3) = Self::grid_search_theta(&x_train, &y_train);
-        let kernel = Self::kernel_mat(&x_train, t1, t2, t3);
+        let kernel = self.kernel_mat(&x_train);
         let kernel_lu = kernel.lu();
 
         let kernel_y = kernel_lu.solve(&y_train).unwrap();
@@ -49,27 +41,44 @@ impl GaussianPredictor {
             for i in 0..train_len {
                 let xi = x_train.row(i).transpose();
                 let xj = x_pred.row(j).transpose();
-                let k = Self::kernel(&xi, &xj, t1, t2, t3, i, j + train_len);
+                let k = self.kernel(&xi, &xj, i, j + train_len);
                 kernel_vec.push(k);
             }
 
             let kernel = DVector::from_vec(kernel_vec);
             let xj = x_pred.row(j).transpose();
-            let k = Self::kernel(&xj, &xj, t1, t2, t3, j + train_len, j + train_len);
+            let k = self.kernel(&xj, &xj, j + train_len, j + train_len);
 
             mean.push(kernel.dot(&kernel_y));
             variance.push(k - (kernel_y.transpose() * kernel_lu.solve(&kernel_y).unwrap())[(0, 0)]);
         }
 
-        // 平均を引いていたのでその分足す
-        for y in mean.iter_mut() {
-            *y += y_average;
-        }
+        Self::postprocess(mean, variance, y_average)
+    }
 
+    fn preprocess(&self) -> (DMatrix<f64>, DVector<f64>, f64) {
+        let x_train = DMatrix::from_row_slice(
+            self.x_list.len(),
+            self.x_list[0].len(),
+            &self.x_list.iter().flatten().copied().collect_vec(),
+        );
+        let mut y_train = DVector::from_vec(self.y_list.clone());
+
+        // 平均を引く
+        let y_average = y_train.mean();
+        y_train.add_scalar_mut(-y_average);
+        (x_train, y_train, y_average)
+    }
+
+    fn postprocess(mut mean: Vec<f64>, variance: Vec<f64>, average: f64) -> (Vec<f64>, Vec<f64>) {
+        // 平均を足す
+        for y in mean.iter_mut() {
+            *y += average;
+        }
         (mean, variance)
     }
 
-    fn kernel_mat(x: &DMatrix<f64>, t1: f64, t2: f64, t3: f64) -> DMatrix<f64> {
+    fn kernel_mat(&self, x: &DMatrix<f64>) -> DMatrix<f64> {
         let n = x.len();
         let mut kernel = DMatrix::zeros(n, n);
 
@@ -77,29 +86,21 @@ impl GaussianPredictor {
             for j in 0..n {
                 let x0 = x.row(i).transpose();
                 let x1 = x.row(j).transpose();
-                kernel[(i, j)] = Self::kernel(&x0, &x1, t1, t2, t3, i, j)
+                kernel[(i, j)] = self.kernel(&x0, &x1, i, j);
             }
         }
 
         kernel
     }
 
-    fn kernel(
-        x0: &DVector<f64>,
-        x1: &DVector<f64>,
-        t1: f64,
-        t2: f64,
-        t3: f64,
-        i: usize,
-        j: usize,
-    ) -> f64 {
+    fn kernel(&self, x0: &DVector<f64>, x1: &DVector<f64>, i: usize, j: usize) -> f64 {
         assert!(x0.shape().0 == 1);
         assert!(x1.shape().0 == 1);
         let diff = x0 - x1;
         let norm = diff.component_mul(&diff)[(0, 0)];
-        let mut kernel = t1 * (-norm / t2).exp();
+        let mut kernel = self.params.theta1 * (-norm / self.params.theta2).exp();
         if i == j {
-            kernel += t3;
+            kernel += self.params.theta3;
         }
         kernel
     }
@@ -111,24 +112,19 @@ impl GaussianPredictor {
         -kernel.determinant().max(1e-100).ln() - (y.transpose() * (kernel_inv * y))[(0, 0)]
     }
 
-    fn grid_search_theta(x: &DMatrix<f64>, y: &DVector<f64>) -> (f64, f64, f64) {
+    pub fn grid_search_theta(&mut self, t1_cands: &[f64], t2_cands: &[f64], t3_cands: &[f64]) {
         let mut best_t1 = std::f64::NAN;
         let mut best_t2 = std::f64::NAN;
         let mut best_t3 = std::f64::NAN;
         let mut best_liklihood = std::f64::MIN;
+        let (x, y, _) = self.preprocess();
 
-        for t1_pow in 4..8 {
-            let t1 = (2.0f64).powi(t1_pow);
-
-            for t2_pow in 3..6 {
-                // 分散なので標準偏差の2乗
-                let t2 = (2.0f64).powi(t2_pow);
-                let t2 = t2 * t2;
-
-                for t3_pow in 0..4 {
-                    let t3 = (2.0f64).powi(t3_pow);
-                    let kernel = Self::kernel_mat(x, t1, t2, t3);
-                    let likelihood = Self::kernel_likelihood(&kernel, y);
+        for &t1 in t1_cands {
+            for &t2 in t2_cands {
+                for &t3 in t3_cands {
+                    self.params = GaussianParam::new(t1, t2, t3);
+                    let kernel = self.kernel_mat(&x);
+                    let likelihood = Self::kernel_likelihood(&kernel, &y);
 
                     if best_liklihood.change_max(likelihood) {
                         best_t1 = t1;
@@ -139,6 +135,23 @@ impl GaussianPredictor {
             }
         }
 
-        (best_t1, best_t2, best_t3)
+        self.params = GaussianParam::new(best_t1, best_t2, best_t3);
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct GaussianParam {
+    theta1: f64,
+    theta2: f64,
+    theta3: f64,
+}
+
+impl GaussianParam {
+    fn new(theta1: f64, theta2: f64, theta3: f64) -> Self {
+        Self {
+            theta1,
+            theta2,
+            theta3,
+        }
     }
 }
