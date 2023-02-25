9b28116da828d5a2de25c4d7c8c50027bed35516
terry-u16 <terry-u16@users.noreply.github.com>
2023-02-26 07:06:50 +0900

    ランダムポリシーを削除

--- src/solver/first.rs
+++ src/solver/first.rs
@@ -1,9 +1,5 @@
-use rand::Rng;
-use rand_pcg::Pcg64Mcg;
-
-use crate::{common::grid::Coordinate, input::Input, map::MapState};
-
 use super::{IncreasingPolicy, Policy, Strategy};
+use crate::input::Input;
 
 pub struct RandomBoringStrategy {
     stage: usize,
@@ -35,43 +31,17 @@ impl RandomBoringStrategy {
 
         policies
     }
-
-    fn get_random_poicies(&self, input: &Input, map: &MapState) -> Vec<Box<dyn Policy>> {
-        const KEEP_OUT_DIST: usize = 60;
-        const TARGET_COUNT: usize = 15;
-        let remain = TARGET_COUNT - (input.water_count + input.house_count);
-        let mut rng = Pcg64Mcg::new(42);
-        let mut policies: Vec<Box<dyn Policy>> = vec![];
-        let mut digged = map.digged.clone();
-
-        let mut trial = 0;
-        const MAX_TRIAL: usize = 1000;
-
-        while policies.len() < remain && trial < MAX_TRIAL {
-            trial += 1;
-            let row = rng.gen_range(0, input.map_size);
-            let col = rng.gen_range(0, input.map_size);
-            let c = Coordinate::new(row, col);
-            if !digged.has_digged_nearby(c, KEEP_OUT_DIST) {
-                policies.push(Box::new(IncreasingPolicy::new(c)));
-                digged.dig(c);
-            }
-        }
-
-        policies
-    }
 }
 
 impl Strategy for RandomBoringStrategy {
     fn get_next_policies(
         &mut self,
         input: &crate::input::Input,
-        map: &mut crate::map::MapState,
+        _map: &mut crate::map::MapState,
     ) -> Vec<Box<dyn super::Policy>> {
         let result = match self.stage {
             0 => self.get_water_poicies(input),
             1 => self.get_house_poicies(input),
-            2 => self.get_random_poicies(input, map),
             _ => vec![],
         };
 
--- src/solver/second.rs
+++ src/solver/second.rs
@@ -1,3 +1,7 @@
+use std::cmp::Reverse;
+
+use itertools::Itertools;
+
 use crate::solver::IncreasingPolicy;
 
 use super::{steiner_tree::calc_steiner_tree_paths, Strategy};
@@ -22,7 +26,7 @@ impl Strategy for SkippingPathStrategy {
         input: &crate::input::Input,
         map: &mut crate::map::MapState,
     ) -> Vec<Box<dyn super::Policy>> {
-        const DIST_SERIES: [usize; 10] = [20, 19, 18, 17, 16, 15, 15, 15, 15, 15];
+        const DIST_SERIES: [usize; 10] = [30, 27, 24, 21, 18, 15, 15, 15, 15, 15];
         if self.iter >= DIST_SERIES.len() {
             self.is_completed = true;
             return vec![];
@@ -34,10 +38,16 @@ impl Strategy for SkippingPathStrategy {
         map.update_prediction(&input);
 
         let paths = calc_steiner_tree_paths(input, map, -0.3);
+        let mut candidates = paths.iter().flatten().copied().collect_vec();
+        candidates.sort_by_cached_key(|&c| {
+            let stddev = map.get_pred_sturdiness(c, 0.0) - map.get_pred_sturdiness(c, -1.0);
+            Reverse(stddev)
+        });
+
         let mut digged = map.digged.clone();
         let mut policies: Vec<Box<dyn super::Policy>> = vec![];
 
-        for &c in paths.iter().flatten() {
+        for &c in candidates.iter() {
             if !digged.has_digged_nearby(c, near_threshold) {
                 digged.dig(c);
                 policies.push(Box::new(IncreasingPolicy::new(c)));
