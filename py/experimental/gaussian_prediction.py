e363a75ca572541257a09263c6295a438469b14d
terry-u16 <terry-u16@users.noreply.github.com>
2023-02-26 15:40:02 +0900

    公開用にビジュアライザをカスタマイズ

--- py/experimental/gaussian_prediction.py
+++ py/experimental/gaussian_prediction.py
@@ -3,6 +3,7 @@ import time
 from typing import List, Tuple
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 import scipy.linalg as linalg
 from PIL import Image
@@ -10,23 +11,19 @@ from PIL import Image
 RAW_SIZE = 200
 SIZE = 40
 STRIDE = 200 // SIZE
-IMG_NO = 148
+IMG_NO = 16
 CNN_PATH = "data/nn_history/20230221_0037_学習データをnumpyに変更"
 
 
 def read_image(path: str) -> np.ndarray:
-    array = np.zeros((SIZE, SIZE), dtype=np.float64)
+    array = np.zeros((RAW_SIZE, RAW_SIZE), dtype=np.float64)
     with open(path) as f:
         _ = f.readline()
 
         for row in range(RAW_SIZE):
-            row //= STRIDE
             line = list(map(int, f.readline().split()))
             for col, v in enumerate(line):
-                col //= STRIDE
                 array[row, col] += v
-
-    array /= STRIDE * STRIDE
 
     return array
 
@@ -38,7 +35,7 @@ def read_sampling_points() -> List[Tuple[int, int]]:
     for i in range(SIZE):
         for j in range(SIZE):
             if image[i, j] == 255:
-                points.append((i, j))
+                points.append((i * STRIDE, j * STRIDE))
 
     return points
 
@@ -83,7 +80,7 @@ def grid_search_theta(x: np.matrix, y: np.matrix) -> Tuple[float, float, float]:
     for t1_pow in range(3, 10):
         t1 = math.pow(2.0, t1_pow)
         for t2 in range(2, 12):
-            t2 = t2 * t2
+            t2 = t2 * t2 * 5 * 5
             for t3_pow in range(0, 5):
                 t3 = math.pow(2.0, t3_pow)
                 k = kernel_mat(x, t1, t2, t3)
@@ -138,35 +135,60 @@ def gaussian_process_regression(
     return mu, var
 
 
-def plot(y_truth: np.matrix, y_mu: np.matrix, y_lower: np.matrix, y_upper: np.matrix):
-    fig = plt.figure(figsize=(16, 8))
+def plot(
+    y_truth: np.matrix,
+    y_mu: np.matrix,
+    y_lower: np.matrix,
+    y_upper: np.matrix,
+    points: List[Tuple[int, int]],
+):
+    fig = plt.figure(figsize=(17, 9))
+
+    x = []
+    y = []
+    for i, j in points:
+        x.append(j)
+        y.append(i)
+
     ax = fig.add_subplot(2, 3, 1)
+    ax.scatter(x, y, marker="o", s=5)
+    ax.set_title("sampling points")
+    ax.set_xlim([0, 250])
+    ax.set_ylim([0, 200])
+    ax.add_patch(patches.Rectangle(xy=(200, 0), width=50, height=200, fc="k"))
+
+    ax = fig.add_subplot(2, 3, 2)
     heatmap = ax.pcolor(y_truth, cmap="jet")
     heatmap.set_clim([0, 5000])
     fig.colorbar(heatmap)
+    ax.set_title("ground truth")
 
     path = f"{CNN_PATH}/pred/{IMG_NO + 0:0>4}.bmp"
     nn_pred = Image.open(path)
     nn_pred = np.array(nn_pred, dtype=np.float64) * 5000 / 255
-    ax = fig.add_subplot(2, 3, 2)
+    ax = fig.add_subplot(2, 3, 3)
     heatmap = ax.pcolor(nn_pred, cmap="jet")
     heatmap.set_clim([0, 5000])
     fig.colorbar(heatmap)
+    ax.set_title("CNN (U-Net)")
 
     ax = fig.add_subplot(2, 3, 4)
     heatmap = ax.pcolor(y_lower, cmap="jet")
     heatmap.set_clim([0, 5000])
     fig.colorbar(heatmap)
+    ax.set_title("gaussian process (-1.0 sigma)")
 
     ax = fig.add_subplot(2, 3, 5)
     heatmap = ax.pcolor(y_mu, cmap="jet")
     heatmap.set_clim([0, 5000])
     fig.colorbar(heatmap)
+    ax.set_title("gaussian process (mean)")
 
     ax = fig.add_subplot(2, 3, 6)
     heatmap = ax.pcolor(y_upper, cmap="jet")
     heatmap.set_clim([0, 5000])
     fig.colorbar(heatmap)
+    ax.set_title("gaussian process (+1.0 sigma)")
 
     plt.show()
 
@@ -182,6 +204,7 @@ def f(x: float, y: float) -> float:
 
 x_train = read_sampling_points()
 y_train = []
+x_train_list = x_train
 print(f"sampling points: {len(x_train)}")
 
 for i, j in x_train:
@@ -197,22 +220,28 @@ y_train = np.power(np.matrix(y_train).T, 1 / POWER_RATIO)
 y_mean = y_train.mean()
 y_train -= y_mean
 
-x_test = np.matrix(range(SIZE * SIZE * 2), dtype=np.float64).reshape(SIZE * SIZE, 2)
+x_test = np.matrix(range(RAW_SIZE * RAW_SIZE * 2), dtype=np.float64).reshape(
+    RAW_SIZE * RAW_SIZE, 2
+)
 
-for i in range(SIZE):
-    for j in range(SIZE):
-        x_test[i * SIZE + j, 0] = i
-        x_test[i * SIZE + j, 1] = j
+for i in range(RAW_SIZE):
+    for j in range(RAW_SIZE):
+        x_test[i * RAW_SIZE + j, 0] = i
+        x_test[i * RAW_SIZE + j, 1] = j
 
 (y_pred_mu, y_pred_var) = gaussian_process_regression(x_test, x_train, y_train)
 y_pred_mu += y_mean
 y_pred_std = np.sqrt(np.where(y_pred_var >= 0, y_pred_var, 0))
 y_pred_lower = y_pred_mu - y_pred_std
 y_pred_upper = y_pred_mu + y_pred_std
-y_pred_mu = np.power(np.array(y_pred_mu).reshape((SIZE, SIZE)), POWER_RATIO)
-y_pred_lower = np.power(np.array(y_pred_lower).reshape((SIZE, SIZE)), POWER_RATIO)
-y_pred_upper = np.power(np.array(y_pred_upper).reshape((SIZE, SIZE)), POWER_RATIO)
+y_pred_mu = np.power(np.array(y_pred_mu).reshape((RAW_SIZE, RAW_SIZE)), POWER_RATIO)
+y_pred_lower = np.power(
+    np.array(y_pred_lower).reshape((RAW_SIZE, RAW_SIZE)), POWER_RATIO
+)
+y_pred_upper = np.power(
+    np.array(y_pred_upper).reshape((RAW_SIZE, RAW_SIZE)), POWER_RATIO
+)
 y_pred_lower = np.where(y_pred_lower >= 10, y_pred_lower, 10)
 y_pred_upper = np.where(y_pred_upper <= 5000, y_pred_upper, 5000)
 
-plot(image, y_pred_mu, y_pred_lower, y_pred_upper)
+plot(image, y_pred_mu, y_pred_lower, y_pred_upper, x_train_list)
